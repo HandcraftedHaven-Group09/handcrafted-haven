05218af2e49b12057efada743434fcc6da8c8484
Brian Crews <bdcrews@protonmail.com>
2024-10-07 14:07:40 -0700

    Test page update

--- src/app/dbtest/page.tsx
+++ src/app/dbtest/page.tsx
@@ -3,16 +3,51 @@ import { getProductsAll } from "@/app/lib/data";
 export default async function Page() {
   const testProduct = await getProductsAll(5);
   return (
-    <main>
+    <main style={{ padding: "1rem" }}>
+      <h2>Database Testing Sandbox</h2>
+
+      <form>
+        <fieldset style={{ padding: "1rem" }}>
+          <legend>PRODUCT</legend>
+          <dl>
+            <dl>
+              <label>Name</label>
+            </dl>
+            <dd>
+              <input type="text"></input>
+            </dd>
+            <dl>
+              <label>Price</label>
+            </dl>
+            <dd>
+              <input type="text"></input>
+            </dd>
+            <dl>
+              <label>Discount Percent</label>
+            </dl>
+            <dd>
+              <input type="text"></input>
+            </dd>
+            <dl>
+              <label>Discount Absolute</label>
+            </dl>
+            <dd>
+              <input type="text"></input>
+            </dd>
+            <dl>
+              <label>Description</label>
+            </dl>
+            <dd>
+              <input type="text"></input>
+            </dd>
+          </dl>
+        </fieldset>
+      </form>
       <ul>
         {testProduct?.map((product) => (
           <li key={product.id}>{product.name}</li>
         ))}
       </ul>
-      <form>
-        <label></label>
-        <input></input>
-      </form>
     </main>
   );
 }
