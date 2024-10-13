e5d43904f1f15f46e2e51b8311b4ca8ee6cc1c44
crewsbd <bdcrews@protonmail.com>
2024-10-13 01:16:27 -0700

    Finished basic data seeding

--- src/app/dbtest/seed/page.tsx
+++ src/app/dbtest/seed/page.tsx
@@ -1,70 +1,148 @@
 import seedData from "@/app/lib/seed.json";
-import { Prisma, PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient, PaymentType, ShippingType } from "@prisma/client";
 
-console.log("Writing need seed data");
-const client = new PrismaClient();
+async function main() {
+  console.log("Writing needed seed data");
+  const client = new PrismaClient();
 
-// Order is important
-client.image.deleteMany({}).catch((e) => console.log(e));
-client.product.deleteMany({});
-client.seller.deleteMany({});
+  // Order is important
+  await client.image.deleteMany({}).catch((e) => console.log(e));
+  await client.user.deleteMany({}).catch((e) => console.log(e));
+  await client.seller.deleteMany({}).catch((e) => console.log(e));
+  await client.product.deleteMany({}).catch((e) => console.log(e));
+  await client.review.deleteMany({}).catch((e) => console.log(e));
+  await client.paymentMethod.deleteMany({}).catch((e) => console.log(e));
+  await client.invoice.deleteMany({}).catch((e) => console.log(e));
 
-for (const seller of seedData.Sellers) {
-  client.seller
-    .create({
-      data: {
-        displayName: seller.displayName,
-        firstName: seller.firstName,
-        lastName: seller.lastName,
-      },
-    })
-    .then(() => {
-      console.log("Sellers created");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // Reset all the auto increment ids
+  await client.$executeRawUnsafe('ALTER SEQUENCE "Image_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "User_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "Seller_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "Product_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "Review_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "PaymentMethod_id_seq" RESTART WITH 1');
+  await client.$executeRawUnsafe('ALTER SEQUENCE "Invoice_id_seq" RESTART WITH 1');
+
+  for (const seller of seedData.Sellers) {
+    await client.seller
+      .create({
+        data: seller,
+      })
+      .then(() => {
+        console.log("Sellers created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const image of seedData.Images) {
+    await client.image
+      .create({
+        data: image,
+      })
+      .then(() => {
+        console.log("Images created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const user of seedData.Users) {
+    await client.user
+      .create({
+        data: user,
+      })
+      .then(() => {
+        console.log("Users created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const product of seedData.Products) {
+    await client.product
+      .create({
+        data: product
+      })
+      .then(() => {
+        console.log("Products created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const review of seedData.Reviews) {
+    await client.review
+      .create({
+        data: review
+      })
+      .then(() => {
+        console.log("Reviews created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const paymentMethod of seedData.PaymentMethods) {
+    await client.paymentMethod
+      .create({
+        // data: paymentMethod
+        data: {
+          identifier: paymentMethod.identifier,
+          expirationMonth: paymentMethod.expirationMonth,
+          expirationYear: paymentMethod.expirationYear,
+          paymentType: paymentMethod.paymentType as PaymentType, //CURSE YOU ENUM!
+          street1: paymentMethod.street1,
+          street2: paymentMethod.street2,
+          city: paymentMethod.city,
+          state: paymentMethod.state,
+          zip: paymentMethod.zip,
+          validation: paymentMethod.validation,
+          userId: paymentMethod.userId
+        }
+      })
+      .then(() => {
+        console.log("PaymentMethods created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  for (const invoice of seedData.Invoices) {
+    await client.invoice
+      .create({
+        // data: paymentMethod
+        data: {
+          subtotal: invoice.subtotal,
+          tax: invoice.tax,
+          shipping: invoice.shipping,
+          shippingType: invoice.shippingType as ShippingType,
+          totalCost: invoice.totalCost,
+          userId: invoice.userId,
+          paymentMethodId: invoice.paymentMethodId
+        }
+      })
+      .then(() => {
+        console.log("Invoices created");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
+
+  console.log("Seeding completed");
+  client.$disconnect();
 }
 
-for (const image of seedData.Images) {
-  client.image
-    .create({
-      data: {
-        url: image.url,
-        description: image.description,
-        ownerId: image.ownerId,
-      },
-    })
-    .then(() => {
-      console.log("Images created");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
+if (process.env.DEV) { main(); }
 
-for (const user of seedData.Users) {
-  client.user
-    .create({
-      data: {
-        userId: user.userId,
-        displayName: user.displayName,
-        firstName: user.firstName,
-        lastName: user.lastName,
-        bio: user.bio,
-        profilePictureId: user.profilePictureId,
-        credential: user.credential,
-      },
-    })
-    .then(() => {
-      console.log("Users created");
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-}
 
-client.$disconnect();
 export default async function Page() {
   return (
     <>
