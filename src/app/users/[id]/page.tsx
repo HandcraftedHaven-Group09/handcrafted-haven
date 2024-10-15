6d3a9c067a7008b1f327592e1b3e68fc5c6b562d
crewsbd <bdcrews@protonmail.com>
2024-10-15 15:40:32 -0700

    User routes init

---
+++ src/app/users/[id]/page.tsx
@@ -0,0 +1,14 @@
+import { User } from 'prisma/prisma-client'
+import { getUserById } from '@/app/lib/data';
+
+import UserBio from "@/app/ui/users/user-bio"
+
+export default async function Page({ params }: { params: { id: string } }) {
+
+    const user = await getUserById(Number(params.id)) as User
+
+    return <>
+        <UserBio user={user}></UserBio>
+    </>
+
+}
