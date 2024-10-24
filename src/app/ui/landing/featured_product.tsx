c6322934264899c58b02a96fdf03d4196c713be5
crewsbd <bdcrews@protonmail.com>
2024-10-24 00:22:57 -0700

    Some styles to make it read better

--- src/app/ui/landing/featured_product.tsx
+++ src/app/ui/landing/featured_product.tsx
@@ -1,18 +1,34 @@
 import { getProductWithImageById } from '@/app/lib/data';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default async function FeaturedProduct({ id }: { id: number }) {
   const product = await getProductWithImageById(id);
   return (
-    <div>
-      <h3>{product?.name}</h3>
+    <Link
+      href={`/products/${id}`}
+      style={{ display: 'grid', gridTemplateColumns: '1fr', maxWidth: '200px' }}
+    >
+      <h3
+        style={{
+          gridColumn: '1/2',
+          gridRow: '1/2',
+          zIndex: '4',
+          padding: '.5rem',
+          textShadow: '0 0 10px black',
+          margin: '0',
+        }}
+      >
+        {product?.name}
+      </h3>
       <Image
+        style={{ gridColumn: '1/2', gridRow: '1/2', zIndex: '2' }}
         src={product?.image.url || ''}
         alt={product?.image.description || ' '}
         width={200}
         height={200}
         unoptimized
       />
-    </div>
+    </Link>
   );
 }
