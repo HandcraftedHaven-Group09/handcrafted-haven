c11cfecd6d636aac67f4841325c4aad127995008
Brian Crews <89043953+crewsbd@users.noreply.github.com>
2024-10-29 17:32:13 -0700

    Fixed key error

--- src/app/ui/landing/nav-lists.tsx
+++ src/app/ui/landing/nav-lists.tsx
@@ -3,27 +3,24 @@
 import Link from 'next/link';
 
 const links = [
-    { name: 'Home', href:'../../' },
-    { name: 'Products', href: '/products'},
-    { name: 'Collections', href:''},
-    { name: 'About', href: '' },
+  { name: 'Home', href: '../../' },
+  { name: 'Products', href: '/products' },
+  { name: 'Collections', href: '' },
+  { name: 'About', href: '' },
 ];
 
 export default function NavLists() {
-    return(
-        <>
-        {links.map((link) => {
-            return (
-                <li>
-                    <Link
-                        key={link.name}
-                         href={link.href}
-                    >
-                    <p>{link.name}</p>
-                    </Link>
-                </li>
-            )
-        })}
-        </>
-    )
+  return (
+    <>
+      {links.map((link) => {
+        return (
+          <li key={link.name}>
+            <Link key={link.name} href={link.href}>
+              <p>{link.name}</p>
+            </Link>
+          </li>
+        );
+      })}
+    </>
+  );
 }
