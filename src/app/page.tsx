aab3aa5ee5d99b7005aa1651f4dad0aa7a14f45c
Brian Crews <89043953+crewsbd@users.noreply.github.com>
2024-10-30 17:12:06 -0700

    Updated some links

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,15 +14,15 @@ export default async function Home() {
         <div id="customers">
           <div>
             <h2>Customers</h2>
-            <Link href="@/login">Login</Link> <br />
-            <Link href="@/customerReg">Registration</Link> <br />
+            <Link href="/users/login">Login</Link> <br />
+            <Link href="/users/signup">Registration</Link> <br />
           </div>
         </div>
         <div id="artisans">
           <div>
             <h2>Artisans</h2>
-            <Link href="@/artisanLogin">Login</Link> <br />
-            <Link href="@/artisanReg">Registration</Link> <br />
+            <Link href="/sellers/login">Login</Link> <br />
+            <Link href="/sellers/signup">Registration</Link> <br />
           </div>
         </div>
       </div>
