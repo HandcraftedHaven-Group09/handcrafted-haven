17d9b36cee57a772fa72785ebb51f391f9d23e81
Brian Crews <89043953+crewsbd@users.noreply.github.com>
2024-11-02 11:52:17 -0700

    Added about page. Tweaked style

---
+++ src/app/about/page.tsx
@@ -0,0 +1,30 @@
+export default function Page() {
+  return (
+    <div style={{ padding: '1rem', width: '100%' }}>
+      <div className="center-children" style={{ maxWidth: '20rem' }}>
+        <h2>About Us</h2>
+        <p style={{ textAlign: 'justify' }}>
+          Welcome to Handcrafted Haven, the online marketplace dedicated to
+          showcasing unique, high-quality handcrafted products from talented
+          artisans and sellers. Our mission is to support independent creators
+          by providing them with a platform to reach a broader audience and
+          share their passion for craftsmanship. We believe in the value of
+          thoughtfully made products that reflect creativity, authenticity, and
+          skilled workmanship. At Handcrafted Haven, we are committed to
+          connecting customers with one-of-a-kind items that cannot be found in
+          mass-market retailers. From handmade jewelry and home decor to
+          specialty art and personalized gifts, our marketplace offers a diverse
+          range of products, each crafted with care and attention to detail. We
+          are proud to support artisans and small business owners who pour their
+          time, effort, and creativity into every piece. Our platform makes it
+          easy for customers to discover and purchase high-quality handcrafted
+          items, while empowering creators to focus on what they do best:
+          creating. Whether you’re an artisan looking to grow your business or a
+          customer seeking unique products, Handcrafted Haven is your
+          destination for exceptional handcrafted goods. Thank you for being a
+          part of our community.
+        </p>
+      </div>
+    </div>
+  );
+}
