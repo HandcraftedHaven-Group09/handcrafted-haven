9ca92a84dbba86959cd88989f5bffd6ddb5c03a2
Livia <livia_macedo89@yahoo.com.br>
2024-11-01 21:45:25 -0300

    test  auth.config

--- src/app/auth.config.ts
+++ src/app/auth.config.ts
@@ -1,5 +1,5 @@
-import type { NextAuthConfig } from 'next-auth';
-import { NextRequest } from 'next/server';
+import type { NextAuthConfig, Session } from 'next-auth'
+import { NextRequest, NextResponse } from 'next/server'
 
 export const authConfig: NextAuthConfig = {
   secret: process.env.AUTH_SECRET,
@@ -7,32 +7,32 @@ export const authConfig: NextAuthConfig = {
     signIn: '/users/login',
   },
   callbacks: {
-    async authorized({ request, auth }: { request: NextRequest; auth: any }) {
-      if (!auth?.user) return false;
-      const userRole: string = auth.user.role;
+    async authorized({ request, auth }: { request: NextRequest; auth: Session | null }) {
+      if (!auth?.user) return false
+      const userRole: string = auth.user.role
 
       // Define restricted paths for each role
       const restrictedPaths: { [key: string]: RegExp[] } = {
         seller: [/^\/products\/?$/, /^\/products\/\d+$/, /^\/products\/cart$/],
         user: [/^\/products\/listing$/, /^\/products\/create$/, /^\/products\/\d+\/edit$/],
-      };
+      }
 
       const isRestricted = (role: string, pathname: string): boolean => {
         return restrictedPaths[role]?.some((path: RegExp) => path.test(pathname))
       }
 
       if (isRestricted(userRole, request.nextUrl.pathname)) {
-        return Response.redirect(new URL('/unauthorized', request.url))
+        return NextResponse.redirect(new URL('/unauthorized', request.url))
       }
 
       return true
     },
     async jwt({ token, user }) {
       if (user) {
-        token.id = user.id;
-        token.role = user.role;
+        token.id = user.id
+        token.role = user.role
       }
-      return token;
+      return token
     },
     async session({ session, token }) {
       session.user.role = token.role
